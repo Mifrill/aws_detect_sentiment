2c7d10526d11485de7f6077e76c1ed0c221f1062
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-08-19 00:25:05 +0500

    initialize gem

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in aws_detect_sentiment.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "rspec", "~> 3.0"
---
+++ spec/aws_detect_sentiment_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AwsDetectSentiment do
+  it "has a version number" do
+    expect(AwsDetectSentiment::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
