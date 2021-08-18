bce6d5f154d50ff31eb7e76f4b612c8b3705d0bd
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-08-19 03:09:33 +0500

    implement configuration logic

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,19 @@
-# frozen_string_literal: true
-
 require "aws_detect_sentiment"
+require "byebug"
+require_relative './support/matchers'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+AwsDetectSentiment.configure do |config|
+  config.stub_client_requests = true
+  config.aws_region = ENV.fetch("AWS_REGION", "region")
+  config.aws_access_key_id = ENV.fetch("AWS_ACCESS_KEY_ID", "key")
+  config.aws_secret_access_key = ENV.fetch("AWS_SECRET_ACCESS_KEY", "secret")
 end
